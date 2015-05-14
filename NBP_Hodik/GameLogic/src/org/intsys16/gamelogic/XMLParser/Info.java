e06b67a5346a827602e3e1d1ee2dd8b406181290
j-june <j-june@192.168.111.64>
2015-05-14 19:21:10 +0400

    Merge origin/master
    Conflicts:
    	HodikGit/src/robots/Unit.java
    	HodikGit/src/robots/bad_robot.java

--- NBP_Hodik/GameLogic/src/org/intsys16/gamelogic/XMLParser/Info.java
+++ NBP_Hodik/GameLogic/src/org/intsys16/gamelogic/XMLParser/Info.java
@@ -7,7 +7,11 @@ package org.intsys16.gamelogic.XMLParser;
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import org.intsys16.gamelogic.FieldControl.Coordinate;
+import org.intsys16.gamelogic.FieldControl.Field;
+import org.intsys16.gamelogic.FieldControl.Field_object;
 //
 /**
  *
@@ -16,10 +20,60 @@ import java.util.List;
 public class Info 
 {
     String robotName;
-    int levelNumber;
-    int x;
-    int y;  
-    int HP;
+    public int levelNumber;
+    public int x;
+    public int y;  
+    public int HP;
     List<mobInfo> mob = new ArrayList();
+    public Info(){};
+    public Info(String name, int ln, Coordinate c, int h)
+    {
+        robotName=name;
+        levelNumber=ln;
+        x=c.getX();
+        y=c.getY();
+        HP=h;
+    }
     
+    public int getLevel()
+    {
+        return levelNumber;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+    
+    public int getHP()
+    {
+        return HP;
+    }
+    
+    public List<mobInfo> getMobs()
+    {
+        return mob;
+    }
+    
+    public void loadMobs(Field a)
+    {
+        HashMap<Coordinate, Field_object> objects=a.getHex();
+        for (Coordinate key : objects.keySet()) {
+            Field_object item=objects.get(key);
+            mobInfo info=new mobInfo();
+            info.type=item.getType();
+            if (!"obstacle".equals(info.type))
+            {
+                info.act_type=item.getActtype();
+                info.hp=item.getDamage();
+            }
+            info.setCoords(item.getCoord());
+            mob.add(info);
+        }
+    }
 }
