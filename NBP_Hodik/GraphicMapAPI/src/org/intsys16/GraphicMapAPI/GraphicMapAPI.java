d6b02f4abf49a534aae22dfb575d6a7717774dfa
grinar <>
2015-06-07 18:33:00 +0300

    Extended abstract public methods for GraphicMapAPI

--- NBP_Hodik/GraphicMapAPI/src/org/intsys16/GraphicMapAPI/GraphicMapAPI.java
+++ NBP_Hodik/GraphicMapAPI/src/org/intsys16/GraphicMapAPI/GraphicMapAPI.java
@@ -17,4 +17,8 @@ public interface GraphicMapAPI {
     }
     public void setParameters(double w, int r, Object gr, Object f);
     public void move(String cmd);
+    public void setEaten(int num);
+    public void setBumbedInto(int num);
+    public void setStepScore(int num);
+    public void deleteFieldObject(int x, int y);
 }
