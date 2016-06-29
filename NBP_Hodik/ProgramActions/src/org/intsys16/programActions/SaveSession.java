8c7f073b367051666b27e00b92ba362440006ccf
eclegend <formrnice@yandex.ru>
2016-06-30 01:55:07 +0300

    Для airad кнопка сохранить сессию рядом с потрясающими кнопками Влада

---
+++ NBP_Hodik/ProgramActions/src/org/intsys16/programActions/SaveSession.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.intsys16.programActions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.intsys16.GameObjectUtilities.AbstractProgram;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Edit",
+        id = "org.intsys16.programActions.SaveSession"
+)
+@ActionRegistration(
+        iconBase = "org/intsys16/programActions/saveSession.png",
+        displayName = "#CTL_SaveSession"
+)
+@ActionReference(path = "Toolbars/File", position = 475)
+@Messages("CTL_SaveSession=Сохранить сессию")
+public final class SaveSession implements ActionListener {
+
+    private final AbstractProgram context;
+
+    public SaveSession(AbstractProgram context) {
+        this.context = context;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        // TODO use context
+    }
+}
